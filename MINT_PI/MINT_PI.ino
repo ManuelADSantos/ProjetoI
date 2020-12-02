eb929e6574fb196f1205ece3ecdd5eb47375d3c2
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-02 04:55:21 +0000

    Update MINT_PI.ino

--- MINT_PI/MINT_PI.ino
+++ MINT_PI/MINT_PI.ino
@@ -22,15 +22,23 @@ Servo S020;
 Servo S050;
 Servo S100;
 Servo S200;
-int servo_pos010, servo_pos020, servo_pos050, servo_pos100, servo_pos200, servo_sinal;
-bool servo_wait;
+int servo_pos010, servo_pos020, servo_pos050, servo_pos100, servo_pos200;
+bool moedas_saida;
 
 //Variáveis utilizadas na moedas_contagem de moedas
 float moedas_contagem = 0;
 float moedas_100 = 0, moedas_200 = 0, moedas_050 = 0, moedas_020 = 0, moedas_010 = 0;
+bool moedas_entrada = false;
 
+//Protótipos de funções
+void contar_moedas();
+void dar_moedas();
 
-//====== SETUP =============================================================
+//!!!!!!!! D&BUGS !!!!!!!!!!!!!!!!
+String servo_sinal, demo_geral, demo_moedas;
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//############## SETUP ############################################################
 void setup()
 {
   Serial.begin(9600);
@@ -56,65 +64,151 @@ void setup()
 
 }
 
-//====== LOOP =============================================================
+//############## LOOP ###########################################################3
 void loop() {
 
-  //====== moedas_contagem DE MOEDAS ======================
-  //Sensor dos 0.10€ ativado (passou moeda)
-  if (analogRead(iv010) >= 1010) {
-    moedas_010 += 0.10;
-    delay(100);
-  }
-  //Sensor dos 0.20€ ativado (passou moeda)
-  if (analogRead(iv020) >= 1010) {
-    moedas_020 += 0.20;
-    delay(100);
-  }
-  //Sensor dos 0.50€ ativado (passou moeda)
-  if (analogRead(iv050) >= 1010) {
-    moedas_050 += 0.50;
-    delay(100);
-  }
-  //Sensor dos 1.00€ ativado (passou moeda)
-  if (analogRead(iv100) >= 1010) {
-    moedas_100 += 1.00;
-    delay(100);
-  }
-  //Sensor dos 2.00€ ativado (passou moeda)
-  if (analogRead(iv200) >= 1010) {
-    moedas_200 += 2.00;
-    delay(100);
+  Serial.println("MENU PRINCIPAL");
+  //====== DEMO ===============================
+  demo_geral = Serial.readString();
+  if (demo_geral == "DEPOSITAR") {
+    moedas_entrada = true;
+  } else if (demo_geral == "LEVANTAR") {
+    moedas_saida = true;
   }
 
-  //====== DEVOLUÇÃO DE MOEDAS ======================
-  //Servo dos 0.10€ ativado (dar moeda)
-  if (analogRead(iv010) < 100) {
-    moedas_010 -= 0.10;
-  }
-  //Servo dos 0.20€ ativado (dar moeda)
-  if (analogRead(iv020) < 100) {
-    moedas_020 -= 0.20;
-  }
-  //Servo dos 0.50€ ativado (dar moeda)
-  if (analogRead(iv050) < 100) {
-    moedas_050 -= 0.50;
-  }
-  //Servo dos 1.00€ ativado (dar moeda)
-  if (analogRead(iv100) < 100) {
-    moedas_100 -= 1.00;
-  }
-  //Servo dos 2.00€ ativado (dar moeda)
-  if (analogRead(iv200) < 100) {
-    moedas_200 -= 2.00;
+  //====== RECEBER MOEDAS ======================
+  if (moedas_entrada == true) {
+    contar_moedas();
   }
 
+  //====== DAR MOEDAS ==========================
+  if (moedas_saida == true) {
+    dar_moedas();
+  }
 
-  //====== MOSTRAR RESULTADOS ======================
-  //Atualizar total
-  moedas_contagem = moedas_100 + moedas_200 + moedas_050 + moedas_020 + moedas_010;
+}
 
-  //Mostrar total atual
-  Serial.print("Montante acumulado: ");
-  Serial.print(moedas_contagem);
-  Serial.println(" €");
+
+//############## FUNÇÕES ###############################################################
+//Função chamada para contar moedas depositadas
+void contar_moedas() {
+  Serial.println("A RECEBER DINHEIRO");
+  while (moedas_entrada == true) {
+    //====== RECEBER MOEDAS ======================
+    //Sensor dos 0.10€ ativado (passou moeda)
+    if (analogRead(iv010) >= 1010) {
+      moedas_010 += 0.10;
+      delay(100);
+    }
+    //Sensor dos 0.20€ ativado (passou moeda)
+    if (analogRead(iv020) >= 1010) {
+      moedas_020 += 0.20;
+      delay(100);
+    }
+    //Sensor dos 0.50€ ativado (passou moeda)
+    if (analogRead(iv050) >= 1010) {
+      moedas_050 += 0.50;
+      delay(100);
+    }
+    //Sensor dos 1.00€ ativado (passou moeda)
+    if (analogRead(iv100) >= 1010) {
+      moedas_100 += 1.00;
+      delay(100);
+    }
+    //Sensor dos 2.00€ ativado (passou moeda)
+    if (analogRead(iv200) >= 1010) {
+      moedas_200 += 2.00;
+      delay(100);
+    }
+
+    //====== MOSTRAR RESULTADOS ======================
+    //Atualizar contagem
+    moedas_contagem = moedas_100 + moedas_200 + moedas_050 + moedas_020 + moedas_010;
+
+    //Mostrar total atual
+    Serial.print("Montante acumulado: ");
+    Serial.print(moedas_contagem);
+    Serial.println(" €");
+
+    if (Serial.available() > 0){
+      demo_moedas = Serial.readString();
+    }
+
+    if (demo_moedas == "STOP_IN") {
+      moedas_entrada = false;
+    }
+  }
+  return;
+}
+
+//Função chamada para contar moedas levantadas
+void dar_moedas() {
+  Serial.println("A DAR DINHEIRO");
+
+  Serial.println("\nSelecione dinheiro a dar:");
+  Serial.println("1 -> 0.10€");
+  Serial.println("2 -> 0.20€");
+  Serial.println("3 -> 0.50€");
+  Serial.println("4 -> 1.00€");
+  Serial.println("5 -> 2.00€\n");
+
+  while(moedas_saida){
+    servo_sinal = Serial.readString();
+    //====== DAR MOEDAS ======================
+    //Servo dos 0.10€ ativado (dar moeda)
+    if (servo_sinal == "1") {
+      Serial.println("A dar moeda de 0.10€");
+      S010.write(180);
+      delay(500);
+      S010.write(0);
+      moedas_010 -= 0.10;
+    }
+    //Servo dos 0.20€ ativado (dar moeda)
+    if (servo_sinal == "2") {
+      Serial.println("A dar moeda de 0.20€");
+      S020.write(180);
+      delay(500);
+      S020.write(0);
+      moedas_020 -= 0.20;
+    }
+    //Servo dos 0.50€ ativado (dar moeda)
+    if (servo_sinal == "3") {
+      Serial.println("A dar moeda de 0.50€");
+      S050.write(180);
+      delay(500);
+      S050.write(0);
+      moedas_050 -= 0.50;
+    }
+    //Servo dos 1.00€ ativado (dar moeda)
+    if (servo_sinal == "4") {
+      Serial.println("A dar moeda de 1.00€");
+      S100.write(180);
+      delay(500);
+      S100.write(0);
+      moedas_100 -= 1.00;
+    }
+    //Servo dos 2.00€ ativado (dar moeda)
+    if (servo_sinal == "5") {
+      Serial.println("A dar moeda de 2.00€");
+      S200.write(180);
+      delay(500);
+      S200.write(0);
+      moedas_200 -= 2.00;
+    }
+
+
+    //====== MOSTRAR RESULTADOS ======================
+    //Atualizar contagem
+    moedas_contagem = moedas_100 + moedas_200 + moedas_050 + moedas_020 + moedas_010;
+
+    //Mostrar total atual
+    Serial.print("Montante acumulado: ");
+    Serial.print(moedas_contagem);
+    Serial.println(" €");
+
+    if (servo_sinal == "STOP_OUT" || moedas_contagem == 0) {
+      moedas_saida = false;
+    }
+  }
+  return;
 }
